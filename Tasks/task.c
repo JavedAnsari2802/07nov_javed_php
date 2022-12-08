f92f4aa31da467a506aa46ddba7e912326a06214
JavedAnsari2802 <118360862+JavedAnsari2802@users.noreply.github.com>
2022-12-08 11:19:47 +0530

    Create task.c

---
+++ Tasks/task.c
@@ -0,0 +1,15 @@
+/* WAP Display This Information using printf 
+1. Your Name 
+2. Your Birth date
+3. Your Age 
+4. Your Address*/
+
+#include<stdio.h>
+
+void main()
+{
+	printf("Name : Javed \n");
+	printf("DOB : 01/01/1989 \n");
+	printf("AGE : 33");
+    printf("Address : krushna kunj, jalaram-2, near indira circle Rajkot 360002");
+ }
